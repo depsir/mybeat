e38ba1563977eb7d94a635db733783201b9a2ce5
Francesco Sacchi <depsir@gmail.com>
2020-02-29 10:43:16 +0100

    Style and auto increment

--- src/components/metronome.js
+++ src/components/metronome.js
@@ -2,15 +2,33 @@ import React, { useState, useEffect, useReducer } from "react"
 import { init, play, scheduler } from "../lib/clicker"
 import { GlobalHotKeys } from "react-hotkeys"
 
+const increase = (bpm=1) => ({
+  type: "INCREASE",
+  value: bpm
+})
+const decrease= (bpm=1) => ({
+  type: "DECREASE",
+  value: bpm
+})
 const Metronome = () => {
   const [started, setStarted] = useState(false)
-  const [bpm, setBpm] = useReducer((state, action) => action === "INCREASE" ? state + 1 : state -1,60)
+  const [autoIncrement, setAutoIncrement] = useState(false)
+  const [bpm, setBpm] = useReducer((state, action) => action.type === "INCREASE" ? state + action.value : state - action.value,60)
 
 
   useEffect(() => {
     init()
   }, [])
 
+
+  useEffect(() => {
+    if (autoIncrement) {
+      let interval = setInterval(() => {
+        setBpm(increase(2))
+      }, 15 * 1000)
+      return () => clearInterval(interval)
+    }
+  }, [autoIncrement])
 
   useEffect(() => {
     if (started) {
@@ -27,19 +45,24 @@ const Metronome = () => {
     setStarted(v)
   }
   const keyMap = { INCREASE: ["+", "shift++"], DECREASE: "-"}
-  const handlers = {INCREASE: () => setBpm("INCREASE"), DECREASE: () => setBpm("DECREASE")}
+  const handlers = {INCREASE: () => setBpm(increase()), DECREASE: () => setBpm(decrease())}
 
 
-  return <div>
+  return <div style={{display: 'flex', flexDirection:"column", alignItems: "center"}}>
     <GlobalHotKeys
       keyMap={keyMap}
       handlers={handlers}
     />
+    <h3>My Beats</h3>
 
-    current bpm: {bpm}
-    <button onClick={() => setBpm("INCREASE")}>+</button>
-    <button onClick={() => setBpm("DECREASE")}>-</button>
+    <div style={{marginBottom: "5px"}}>
+      <button onClick={() => setBpm(increase())}>+</button>
+      <span style={{margin: "0 5px"}}>{bpm}</span>
+      <button onClick={() => setBpm(decrease())}>-</button>
+    </div>
+
     <button onClick={() => doStart(!started)}>{started ? "stop" : "start"}</button>
+    <button onClick={() => setAutoIncrement(!autoIncrement)}>{"autoIncrement"}</button>
   </div>
 }
 
