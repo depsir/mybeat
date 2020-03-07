8de295782ab89817117455d8339061901519fccf
Francesco Sacchi <depsir@gmail.com>
2020-03-07 22:55:13 +0100

    Better experience for schedule set on mobile

--- src/components/incrementSelector.js
+++ src/components/incrementSelector.js
@@ -23,23 +23,33 @@ const useStyles = makeStyles({
   }
 })
 
+const NumberInput = ({value, onChange}) => {
+  const classes = useStyles()
+  const handleChange = e => {
+    const input = parseInt(e.target.value, 10);
+    if (input <= 0) return
+    onChange(input)
+  }
+  return <Input error={!(value > 0)} className={classes.numberInput} value={value} onChange={handleChange} type={"number"}/>
+}
+
 const IncrementSelector = (props) => {
   const { delta, period, direction, mode, unit } = props.auto
-  const setDelta = s => s > 0 && props.setAuto({...props.auto, delta: s})
+  const setDelta = s => {props.setAuto({...props.auto, delta: s})}
   const setUnit = s => props.setAuto({...props.auto, unit: s})
-  const setPeriod = s => s > 0 && props.setAuto({...props.auto, period: s})
+  const setPeriod = s => props.setAuto({...props.auto, period: s})
   const setDirection = s => props.setAuto({...props.auto, direction: s})
   const setMode = s => props.setAuto({ ...props.auto, mode: s })
   const classes = useStyles()
 
   return <Col>
-    <div style={{minWidth:"230px", textAlign: "center"}}>{`${direction > 0 ? "Increase" : 'Decrease'} ${delta} ${unit} after ${period} ${mode === INCREMENT.TIME ? "seconds" : "measures"}`}</div>
+    <div style={{minWidth:"230px", textAlign: "center"}}>{`${direction > 0 ? "Increase" : 'Decrease'} ${delta || "-"} ${unit} after ${period || '-'} ${mode === INCREMENT.TIME ? "seconds" : "measures"}`}</div>
     <Row className={classes.spaced}>
       {direction > 0 ? <Grow color="primary" onClick={() => setDirection(-1)}/> : <Lower color="primary" onClick={() => setDirection(1)}/>}
-      <Input className={classes.numberInput} value={delta} onChange={e => setDelta(parseInt(e.target.value, 10))} type={"number"}/>
+      <NumberInput value={delta} onChange={e => setDelta(e)} />
       {unit === INCREMENT_UNIT.BPM ? <span onClick={()=>setUnit(INCREMENT_UNIT.PERCENT)}>b</span>:<span onClick={()=>setUnit(INCREMENT_UNIT.BPM)}>%</span>}
       <span>after</span>
-      <Input className={classes.numberInput} value={period} onChange={e => setPeriod(parseInt(e.target.value))} type={"number"}/>
+      <NumberInput value={period} onChange={e => setPeriod(e)} />
       {mode === INCREMENT.TIME ? <TimeMode color="primary" onClick={() => setMode(INCREMENT.BEAT)}/> : <BeatMode color="primary" onClick={() => setMode(INCREMENT.TIME)}/>}
     </Row>
   </Col>
